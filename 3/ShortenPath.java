ec6762501a4c6c770c79c1ad794313fdf4ffd2eb
Sankalp Sharma <sansharma056@gmail.com>
2021-09-21 11:57:50 +0530

    using stack instead of tree for ShortenPath

--- 3/ShortenPath.java
+++ 3/ShortenPath.java
@@ -1,72 +1,40 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Deque;
+import java.util.ArrayDeque;
 
 class ShortenPath {
 	public static String shortenPath(String path) {
 		String pathElements[] = path.split("/");
-		StringBuilder shortestPath = new StringBuilder(); 
-
-		Tree tree = new Tree();	
-		Tree.Node cur = null;	
+		StringBuilder shortestPath = new StringBuilder();
+	
+		Deque<String> stack = new ArrayDeque<>();
 
 		for(String ele : pathElements) {
-			if(tree.root == null) {
-				tree.root = new Tree.Node(null, ele);
-				cur = tree.root;
-			} else if(ele.equals("..")) {
-					if(cur.parent != null) {
-						cur = cur.parent;	
-					}
-			} else if(ele.equals(".") || ele.equals("")) {
-					continue;
-			}	else {
-				if(!cur.children.containsKey(ele)) {
-					cur.children.put(ele, new Tree.Node(cur, ele));
+			if(ele.equals("..")) {
+				if(!stack.isEmpty()) {
+					stack.pop();
 				}
-
-				cur = cur.children.get(ele);
+			} else if(!ele.equals(".") && !ele.equals("")) {
+					stack.push(ele);
 			}
 		}
 		
-		if(cur == null) return "/";
+		if(stack.isEmpty()) {
+			return "/";
+		}
+		
+		while(!stack.isEmpty()) {
+			shortestPath.insert(0, stack.pop());
+			shortestPath.insert(0, "/");
+		}
 
-		while(cur.parent != null) {
-			if(shortestPath.length() != 0) {
-				shortestPath.insert(0, "/");
-				shortestPath.insert(0, cur.data);
-			} else {
-					shortestPath.append(cur.data);
-			}
-
-			cur = cur.parent;
-		} 
-			
-		shortestPath.insert(0, "/");
-
-		return shortestPath.toString(); 
-	}	
+		return shortestPath.toString();
+	}
 
 	public static void main(String args[]) {
 		String paths[] = {"/home/", "/../", "/home//foo/", "/a/./b/../../c/", "/", "/foo/../test/../test/../foo//bar/./baz"};
 
 		for(String path : paths) {
 			System.out.println(shortenPath(path));
-		}
-	}
-}
-
-class Tree {
-	Node root;
-	
-	static class Node {
-		Node parent;
-		String data;
-		Map<String, Node> children;
-
-		Node(Node parent, String data) {
-			this.parent = parent;
-			this.data = data;
-			children = new HashMap<>();
 		}
 	}
 }
