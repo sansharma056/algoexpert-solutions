c3ae26b3fbf24ba80ba75dc15b3fa4a8db48f1cf
Sankalp Sharma <sansharma056@gmail.com>
2021-10-03 14:35:27 +0530

    added ts code for: NodeDepths

---
+++ 1/js/src/NodeDepths.ts
@@ -0,0 +1,28 @@
+import {TreeNode, Tree} from "./Tree";
+
+function getNodeDepths(tree: Tree): number {
+	if(tree.root == null) {
+		return 0;
+	}
+
+	return getNodeDepthsHelper(tree.root, 0);
+}
+
+function getNodeDepthsHelper(root: TreeNode, depth: number): number {
+	if(root == null) {
+		return 0;
+	}
+
+	return depth + getNodeDepthsHelper(root.left!, depth+1) + getNodeDepthsHelper(root.right!, depth+1); 
+
+}
+
+(function main() {
+	const tree: Tree = new Tree();
+	
+	for(let i=1; i<=9; i++) {
+		tree.insertOne(i);
+	}
+	
+	console.log(getNodeDepths(tree));	
+})();
