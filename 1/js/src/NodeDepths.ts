dec2fee4a8b7ecbdf0ec8657bd4701082cde9aea
Sankalp Sharma <sansharma056@gmail.com>
2021-10-05 20:34:00 +0530

    removed extra spaces after lines in NodeDepths

--- 1/js/src/NodeDepths.ts
+++ 1/js/src/NodeDepths.ts
@@ -13,16 +13,16 @@ function getNodeDepthsHelper(root: TreeNode, depth: number): number {
 		return 0;
 	}
 
-	return depth + getNodeDepthsHelper(root.left!, depth+1) + getNodeDepthsHelper(root.right!, depth+1); 
+	return depth + getNodeDepthsHelper(root.left!, depth+1) + getNodeDepthsHelper(root.right!, depth+1);
 
 }
 
 (function main() {
 	const tree: Tree = new Tree();
-	
+
 	for(let i=1; i<=9; i++) {
 		tree.insertOne(i);
 	}
-	
-	console.log(getNodeDepths(tree));	
+
+	console.log(getNodeDepths(tree));
 })();
