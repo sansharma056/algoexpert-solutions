7513b28b6213acc5892212c6cb313f4a08adf16b
Sankalp Sharma <sansharma056@gmail.com>
2021-10-19 21:34:35 +0530

    rewrote solution for MergeLinkedList.java

--- 3/MergeLinkedList.java
+++ 3/MergeLinkedList.java
@@ -1,51 +1,45 @@
 class MergeLinkedList {
 	// Time Complexity O(n+m), Space Complexity O(1)
-	// Override list
-	public static void mergeAlt(List list1, List list2) {
-		Node ptr1=list1.head, ptr2=list2.head;
-		
-		Node ptr1Prev = null;
-
-		while(ptr1 != null && ptr2 != null) {
-			if(ptr1.data < ptr2.data) {
-				ptr1Prev = ptr1;
-				ptr1 = ptr1.next;
-				continue;
-			}
-			
-			Node ptr2Next = ptr2.next;
-
-			if(ptr1 == list1.head) {
-				list1.head = ptr2;
-				ptr2.next = ptr1;
-				ptr1Prev = list1.head;
-				ptr2 = ptr2Next;
-				continue;
-			}
-			
-			ptr1Prev.next = ptr2;
-			ptr2.next = ptr1;
-			ptr1Prev = ptr2;
-			ptr1 = ptr2; 
-			ptr2 = ptr2Next;
+	public static List mergeAlt(List list1, List list2) {
+		if(list1.head == null) {
+			return list2;
 		}
-		
 
-		while(ptr2 != null) {
-			ptr1Prev.next = ptr2;
-			ptr1Prev = ptr1Prev.next;
-			ptr2 = ptr2.next;
+		if(list2.head == null) {
+			return list1;
 		}
+
+		Node p1 = list1.head, p2 = list2.head, prev = null;
+
+		while(p1 != null && p2 != null) {
+			if(p1.data < p2.data) {
+				prev = p1;
+				p1 = p1.next;
+			}	else {
+				if(prev != null) {
+					prev.next = p2;
+				}
+				prev = p2;
+				p2 = p2.next;
+				prev.next = p1;
+			}
+		}
+
+		if(prev != null) {
+			prev.next = p2;
+		}
+
+		return list1.head.data < list2.head.data ? list1 : list2;
 	}
-		
+
 	// Time Complexity O(n+m), Space Complexity O(n+m)
 	// Create new list
 	public static List merge(List list1, List list2) {
 		List mergedList = new List();
-	
+
 		Node ptr1 = list1.head;
 		Node ptr2 = list2.head;
-		
+
 		while(ptr1 != null && ptr2 != null) {
 			if(ptr1.data < ptr2.data) {
 				mergedList.add(ptr1.data);
@@ -55,12 +49,12 @@ class MergeLinkedList {
 					ptr2 = ptr2.next;
 			}
 		}
-		
+
 		while(ptr1 != null) {
 			mergedList.add(ptr1.data);
 			ptr1 = ptr1.next;
 		}
-		
+
 		while(ptr2 != null) {
 			mergedList.add(ptr2.data);
 			ptr2 = ptr2.next;
@@ -74,17 +68,16 @@ class MergeLinkedList {
 		List list2 = new List();
 
 		for(int x : new int[]{2,6,7,8}) {
-			list2.add(x);
-		}
-		
-		for(int x : new int[]{1,3,4,5,9,10}) {
 			list1.add(x);
 		}
-		
-		System.out.println(merge(list1, list2));
-		mergeAlt(list1, list2);
-		System.out.println(list1);
-	}	
+
+		for(int x : new int[]{1,3,4,5,9,10}) {
+			list2.add(x);
+		}
+
+		System.out.println(mergeAlt(list1, list2));
+		// System.out.println(merge(list1, list2));
+	}
 }
 
 class List {
@@ -93,11 +86,19 @@ class List {
 	List() {
 		head = tail = null;
 	}
-	
+
+	List(Node head) {
+		this.head = tail = head;
+
+		while(tail.next != null) {
+			tail = tail.next;
+		}
+	}
+
 	@Override
 	public String toString() {
 		StringBuffer str = new StringBuffer();
-		
+
 		Node cur = head;
 		while(cur != null) {
 			str.append(cur.data);
@@ -113,7 +114,7 @@ class List {
 			head = tail = new Node(data);
 			return;
 		}
-		
+
 		tail.next = new Node(data);
 		tail = tail.next;
 	}
