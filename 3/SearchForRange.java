41a45cb92df0205e61f64fee457f63381fdaa684
Sankalp Sharma <sansharma056@gmail.com>
2021-10-21 19:46:59 +0530

    Added O(1) space complexity code for SearhForRange.java

--- 3/SearchForRange.java
+++ 3/SearchForRange.java
@@ -3,8 +3,8 @@ class SearchForRange {
 	public static int solve(int arr[], int first, int last, int range[], int target) {
 		if(first > last) {
 			return -1;
-		}	
-		
+		}
+
 		int mid = (first+last) / 2;
 
 		if(arr[mid] == target) {
@@ -15,11 +15,11 @@ class SearchForRange {
 					solve(arr, mid+1, last, range, target );
 			} else if(mid+1 == arr.length || arr[mid+1] != target) {
 					range[1] = mid;
-					solve(arr, first, mid-1, range, target);						
+					solve(arr, first, mid-1, range, target);
 			} else {
-					solve(arr, first, mid-1, range, target);						
+					solve(arr, first, mid-1, range, target);
 					solve(arr, mid+1, last, range, target);
-			}		
+			}
 			return mid;
 		}	else if(target < arr[mid]) {
 				return solve(arr, first, mid-1, range, target);
@@ -31,20 +31,20 @@ class SearchForRange {
 
 	public static int[] solve(int arr[], int target) {
 		int range[] = new int[2];
-		
+
 		int pos = solve(arr, 0, arr.length-1, range, target);
 		if(pos == -1) {
 			return new int[]{-1, -1};
-		}	
+		}
 
 		return range;
-	}	
-	
+	}
+
 
 	public static void main(String args[]) {
 		int arr[] = {0,1,21,33,45,45,45,45,45,45,61,71,73};
 		int target = 45;
-		
+
 		System.out.println(Arrays.toString(solve(arr, target)));
 	}
 }
